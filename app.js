3b2867ca2c49378cab5091fda8efa52ac4be496a
Arti M <artismarti@gmail.com>
2025-05-07 13:53:08 +0100

    test ignore and non ignore together

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Let's test merge queues some more");
+console.log(
+	"This is a test to see if filters work when ignored files are changes along with non ignored ones"
+);
--- ignoreMe.js
+++ ignoreMe.js
@@ -1,2 +1,1 @@
-console.log("Ignore any changes I make to this file!");
 console.log("Time to test this!");
