d3ee6f94b495af9e9c78975316932d15400ca79c
Arti M <artismarti@gmail.com>
2025-05-07 14:21:37 +0100

    test

--- ignoreMe.js
+++ ignoreMe.js
@@ -1,6 +1,1 @@
-console.log(
-	"This is a test to see if filters work when only ignored files are changed"
-);
-console.log(
-	"This is a test to see if filters work when ignored files are changed"
-);
+console.log("words");
